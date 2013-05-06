3f162b59118bfa874c8423cc51d4ef22226f4f59
George Privon <accounts5@privon.com>
2013-05-06 03:19:57 -0400

    doesn't assume first location is away

--- loc_analyze.py
+++ loc_analyze.py
@@ -99,10 +99,21 @@ else:
 
 if mode=='week':
   command='SELECT * FROM locations WHERE WEEK(UTC,1)=%i AND YEAR(UTC)=%i ORDER by locations.UTC' % (week,year)
+  command2='SELECT * FROM locations WHERE WEEK(UTC,1)=%i AND YEAR(UTC)=%i ORDER by locations.UTC DESC' % ((52,year-1),(week-1,year))[week > 1]
+  command3='SELECT * FROM locations WHERE WEEK(UTC,1)=%i AND YEAR(UTC)=%i ORDER by locations.UTC' % ((0,year+1),(week+1,year))[week < 53]
 elif mode=='month':
   command='SELECT * FROM locations WHERE MONTH(UTC)=%i AND YEAR(UTC)=%i ORDER by locations.UTC' % (month,year)
+  command2='SELECT * FROM locations WHERE MONTH(UTC)=%i AND YEAR(UTC)=%i ORDER by locations.UTC DESC' % ((12,year-1),(month-1,year))[month > 1]
+  command3='SELECT * FROM locations WHERE MONTH(UTC)=%i AND YEAR(UTC)=%i ORDER by locations.UTC' % ((1,year+1),(month+1,year))[month < 12]
 cursor.execute(command)
 recs=cursor.fetchall()
+cursor.execute(command2)
+preloc=cursor.fetchone()
+print command2
+cursor.execute(command3)
+postloc=cursor.fetchone()
+
+print preloc
 
 if len(recs)<1:
   sys.stderr.write('ERROR: no records found for the requested time interval. Exiting.\n')
@@ -111,7 +122,7 @@ if len(recs)<1:
 # go through the rows sequentially. First, check if the GPS location puts it
 # within the "home" tolerance specified above. If it is, add that time to the 
 # amount spent at "home".
-rec0=recs[0]
+rec0=preloc
 nrecs=len(recs)
 loctype='away'
 d=0
