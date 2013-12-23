a973a58eb385cc2d1d8e578faf7eb7f8a5fc974b
George Privon <george@privon.com>
2013-12-23 01:28:36 -0500

    better module imports, added sys import

--- magellan.py
+++ magellan.py
@@ -1,22 +1,25 @@
 # magellan library
 
-import math,ConfigParser,MySQLdb
+import math as _math
+import ConfigParser as _ConfigParser
+import MySQLdb as _MySQLdb
+import sys as _sys
 
 # load configuration file and connect to the database
 def initdb():
   # load information from the configuration file
-  config=ConfigParser.RawConfigParser()
+  config=_ConfigParser.RawConfigParser()
   config.read('.magellan')
   if not(config.get('Server Config','server')) or not(config.get('Server Config','user')) or not(config.get('Server Config','password')) or not(config.get('Server Config','db')):
-    sys.stderr.write('Configuration file error. Please check the configuration file.\n')
-    sys.exit(-1)
+    _sys.stderr.write('Configuration file error. Please check the configuration file.\n')
+    _sys.exit(-1)
   else:
     Mserver=config.get('Server Config','server')
     Muser=config.get('Server Config','user')
     Mpw=config.get('Server Config','password')
     Mdb=config.get('Server Config','db')
   # create a catabase connection
-  scon=MySQLdb.connect(host=Mserver,user=Muser,passwd=Mpw,db=Mdb)
+  scon=_MySQLdb.connect(host=Mserver,user=Muser,passwd=Mpw,db=Mdb)
   scur=scon.cursor()
   return scur
 
@@ -24,10 +27,10 @@ def initdb():
 def GreatCircDist(loc1,loc2):
   dlat=loc2[0]-loc1[0]
   dlong=loc2[1]-loc2[1]
-  rdlat=math.radians(dlat)
-  rdlong=math.radians(dlong)
-  ha=math.sin(rdlat/2) * math.sin(rdlat/2) + math.cos(math.radians(loc1[1])) * math.cos(math.radians(loc1[1]))*math.sin(rdlong/2) * math.sin(rdlong/2)
-  hc= 2 * math.atan2(math.sqrt(ha), math.sqrt(1-ha))
+  rdlat=_math.radians(dlat)
+  rdlong=_math.radians(dlong)
+  ha=_math.sin(rdlat/2) * _math.sin(rdlat/2) + _math.cos(_math.radians(loc1[1])) * _math.cos(_math.radians(loc1[1]))*_math.sin(rdlong/2) * _math.sin(rdlong/2)
+  hc= 2 * _math.atan2(_math.sqrt(ha), _math.sqrt(1-ha))
   return 6378.1*hc
 
 def yearid(year,other):
