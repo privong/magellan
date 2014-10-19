6fbd8f3980538679d624ee201210f62fccc2d259
George C. Privon <george@privon.com>
2014-10-18 23:07:32 -0300

    GreatCircDist bugfix

--- magellan.py
+++ magellan.py
@@ -23,7 +23,7 @@ def initdb():
 # compute the great circle distance using the haversine formula
 def GreatCircDist(loc1,loc2):
   dlat=loc2[0]-loc1[0]
-  dlong=loc2[1]-loc2[1]
+  dlong=loc2[1]-loc1[1]
   rdlat=math.radians(dlat)
   rdlong=math.radians(dlong)
   ha=math.sin(rdlat/2) * math.sin(rdlat/2) + math.cos(math.radians(loc1[1])) * math.cos(math.radians(loc1[1]))*math.sin(rdlong/2) * math.sin(rdlong/2)
