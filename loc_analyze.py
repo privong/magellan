dbb0fe3ba3d3a4a821126dc4dd01848c8614b5ca
George C. Privon <accounts5@privon.com>
2017-05-29 03:46:17 +0000

    reformat title comments

--- loc_analyze.py
+++ loc_analyze.py
@@ -1,24 +1,25 @@
 #!/usr/bin/env python2
-#
-# loc_analyze.py
-#
-# Provide a (weekly/monthly) analysis of GPS log information.
-# The information will be retreived from an SQL database
-#
-# Copyright (C) 2014-2015 George C. Privon
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+loc_analyze.py
+
+Provide a (weekly/monthly) analysis of GPS log information.
+The information will be retreived from an SQL database
+
+Copyright (C) 2014-2015, 2017 George C. Privon
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
 
 import MySQLdb
 import sys
--- sql_import.py
+++ sql_import.py
@@ -1,25 +1,26 @@
 #!/usr/bin/env python2
-#
-# sql_import.py
-#
-# Load a textfile (CSV) of GPS data and store it into a SQL database.
-# The log file should have the following format:
-# UTC time,lat,long,horiz accuracy,alt,vert accuracy,speed,heading,battery
-#
-# Copyright (C) 2014-2015 George C. Privon
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+sql_import.py
+
+Load a textfile (CSV) of GPS data and store it into a SQL database.
+The log file should have the following format:
+UTC time,lat,long,horiz accuracy,alt,vert accuracy,speed,heading,battery
+
+Copyright (C) 2014-2015, 2017 George C. Privon
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
 
 
 import MySQLdb
